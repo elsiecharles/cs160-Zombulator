c98a46ece3036ba92d0136e7c78807281c708879
Elsie Charles <charleel@oregonstate.edu>
2017-11-05 20:06:47 -0800

    created isTouching function and began development of handleCollisions

--- sketch.js
+++ sketch.js
@@ -1,53 +1,77 @@
 //Zombulator by Elsie Charles
 //CS160 
 
-
 var backgroundColor;
 
 const MIN_SIZE = 5;
 const MAX_SIZE = 50;
 const POPULATION_SIZE = 500;
 
-
 var population = [];
+
+var zombieCount = 0;
+var humanCount = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   backgroundColor = color(245, 255, 245);
-  initializePopulation ();
+  initializePopulation();
 }
-
 
 function draw() {
   background(backgroundColor);
   noStroke();
-  drawPopulation ();
-  movePopulation ();
+  drawPopulation();
+  movePopulation();
+  drawPopulationCounts();
+  handleCollisions ();
 }
 
-function initializePopulation(){
-  for (var i = 0; i < POPULATION_SIZE; ++i){
-    var humanoid_type = random(0, 100);
-    if (humanoid_type <= 50){
-      population[i] = initializeZombie();
-    } else {
-      population [i] = initializeHuman ();
+function handleCollisions () {
+  for(var i = 0; i < POPULATION_SIZE; ++i) {
+    var attacker = population[i];
+    for (var j = i + 1; j < POPULATION_SIZE; ++j) {
+      var target = population[j];
+
+      if (attacker.isTouching(target)) {
+        print ("Fight");
+      }
     }
   }
 }
 
-function drawPopulation () {
-  for(var i = 0; i <POPULATION_SIZE; ++i) {
-    population[i].draw ();
+function initializePopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    var humanoid_type = random(0, 100);
+    if (humanoid_type <= 50) {
+      population[i] = initializeZombie();
+      ++zombieCount;
+    } else {
+      population[i] = initializeHuman();
+      ++humanCount;
+    }
   }
 }
 
-function movePopulation () {
+function drawPopulationCounts() {
+  stroke(0);
+  textSize(72);
+  textAlign(CENTER);
+  text("Zombies: " + zombieCount, width / 2, 100);
+  text("Humans: " + humanCount, width / 2, height - 100);
+}
+
+function drawPopulation() {
   for (var i = 0; i < POPULATION_SIZE; ++i) {
-    population[i].move ();
+    population[i].draw();
   }
 }
-// Zombies. Raaahh!
+
+function movePopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    population[i].move();
+  }
+}
 
 function initializeZombie() {
   return {
@@ -71,35 +95,38 @@ function initializeZombie() {
     draw: function() {
       fill(this.color);
       ellipse(this.x, this.y, this.size, this.size);
+    },
+    isTouching: function(target){
+
     }
   };
 }
 
-// Humans. Mmmm brains!
-
-
-function initializeHuman(index) {
+function initializeHuman() {
   return {
     x: random(0, windowWidth),
     y: random(windowHeight - 200, windowHeight),
     speed: random(0.25, 3),
     size: random(MIN_SIZE, MAX_SIZE),
     color: color(random(50, 150), random(50, 150), random(150, 255), 150),
-    draw: function() {
-      fill(this.color);
-      ellipse(this.x, this.y, this.size, this.size);
-    },
     move: function() {
-      var direction = random(0, 100);
-      if (direction < 20) {
-        this.x += this.speed;
-      } else if (direction < 40) {
-        this.x -= this.speed;
-      } else if (direction < 60) {
-        this.y += this.speed;
-      } else {
-        this.y -= this.speed;
-      }
+        var direction = random(0, 100);
+        if (direction < 20) {
+          this.x += this.speed;
+        } else if (direction < 40) {
+          this.x -= this.speed;
+        } else if (direction < 60) {
+          this.y += this.speed;
+        } else {
+          this.y -= this.speed;
+        }
+      },
+    draw: function() {
+        fill(this.color);
+        ellipse(this.x, this.y, this.size, this.size);
+    },
+    isTouching: function(target){
+      
     }
-  }
+  };
 }
